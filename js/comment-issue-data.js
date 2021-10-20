e98bd3c9e51bd66bbb6c7733f25231909a45b8a4
egsee <taikox@126.com>
2021-10-20 23:40:17 +0800

    change

--- js/comment-issue-data.js
+++ js/comment-issue-data.js
@@ -1,8 +1,9 @@
 // 评论issues仓库 by.egsee https://egsee.github.io/
-var repoIssuesUrl = "https://api.github.com/repos/egsee/blog_comment/issues";
+var repoIssuesUrl = "https://api.github.com/repos/egsee/public-blog-comments/issues";
 // 对应仓库 clientId、clientSecret 关于这两个参数的安全问题，查看 https://egsee.github.io/2019/09/19/博客源码分享.html#1-热门推荐，最新评论：
-var clientId = "46a9f3481b46ea0129d8";
-var clientSecret = "79c7c9cb847e141757d7864453bcbf89f0655b24";
+var clientId = "fec450a2bb38d0063cc8";
+var clientSecret = "24432e44cfc517df8d522a0679a34f5ada039bd9";
+
 var authorizationToken = 'Basic ' + btoa(clientId + ':' + clientSecret);
 // 写comment count值
 var reqCommentCountUrl = repoIssuesUrl + "?t=" + new Date().getTime() + "&labels=Gitalk,";
